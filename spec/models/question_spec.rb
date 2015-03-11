bf5813248ffe49e944b38ffcbbb5ea0bd1d79504
boutsko <boutsko@gmail.com>
2015-03-11 22:22:00 +0300

    amend check for have_many dependency in one line as per shoulda_matchers docs

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,18 +1,8 @@
 # coding: utf-8
 require 'rails_helper'
 
-RSpec.describe Question, type: :model do
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :body }
+describe Question, type: :model do
+  it { should validate_presence_of :title and :body }
   it { should have_many :answers }
-
-
-  describe "destroying question" do
-    it "should destroy question and all its answers" do
-      question = FactoryGirl.create(:question)
-      answer = FactoryGirl.create(:answer, question: question)
-      answer1 = FactoryGirl.create(:answer, question: question)
-      expect { question.destroy! }.to change{ Answer.count }.by(-2)
-    end
-  end
+  it { should have_many(:answers).dependent(:destroy) }
 end
