86bf930fbc7f48e192654f29763dd04361edd785
boutsko <boutsko@gmail.com>
2015-03-13 00:00:17 +0300

    add tests for #new

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -29,6 +29,19 @@ RSpec.describe QuestionsController, type: :controller do
     it 'renders show view' do
       expect(response).to render_template :show
     end
-    
+
+    describe 'GET #new' do
+
+      before { get :new }
+      
+      it 'assigns new Question to @question ' do
+        expect(assigns(:question)).to be_a_new(Question)
+      end
+
+      it 'renders new view' do
+        expect(response).to render_template :new
+      end
+    end
+
   end
 end
