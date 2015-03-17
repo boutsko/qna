f95b2802b20dbb570a77236d77d06820338f7267
Sam Pai <sampai@sampai-9.local>
2015-03-17 22:23:36 +0300

    add sign_in_user via support/controller_macros.rb

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -32,6 +32,8 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #new' do
+    sign_in_user
+
     before { get :new }
     
     it 'assigns new Question to @question ' do
@@ -45,6 +47,7 @@ RSpec.describe QuestionsController, type: :controller do
 
 
   describe 'GET #edit' do
+    sign_in_user
     before { get :edit, id: question }
 
     it 'assigns requested question to @question' do
@@ -57,6 +60,7 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'POST #create' do
+    sign_in_user
     context 'with valid attributes' do
       it 'saves new question in database'do
         expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by(1)
@@ -81,6 +85,7 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'PATCH #update' do
+    sign_in_user
     context 'valid attributes' do
       it 'assigns the requested question to @question' do
         patch :update, id: question, question: attributes_for(:question)
@@ -115,6 +120,7 @@ RSpec.describe QuestionsController, type: :controller do
     end
 
     describe 'DELETE #destroy' do
+      sign_in_user
       before { question }
       
       it 'deletes question' do
