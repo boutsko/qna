fa7d76d0d6605c4ba02839d7ec67df8a4d3580ad
Sam Pai <sampai@sampai-10.local>
2015-03-23 01:48:21 +0300

    add check for presence of question body ones a question has been created

--- spec/acceptance/questions/create_question_spec.rb
+++ spec/acceptance/questions/create_question_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'Ask question', %q{
-  to receive a piece wisdom form the comunity 
+  to receive a piece of wisdom form the comunity 
   as an authorized member of QnA
   I can ask a question
 } do
@@ -18,12 +18,12 @@ feature 'Ask question', %q{
     fill_in 'Body', with: 'text text'
     click_on 'Create'
     expect(page).to have_content 'Test question'
+    expect(page).to have_content 'text text'
   end
 
-  scenario 'Non-authenticated user created question' do
+  scenario 'Non-authenticated user tries to create question' do
     visit questions_path
     click_on 'Ask question'
-
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
