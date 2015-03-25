01d38aa1bdef6e868bffb0e6004f355919c4fd54
Sam Pai <sampai@sampai-10.local>
2015-03-25 22:31:19 +0300

    add more models validations

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 
 describe Question, type: :model do
   it { should validate_presence_of :title and :body }
-  it { should have_many :answers }
   it { should have_many(:answers).dependent(:destroy) }
+  it { should belong_to(:user) }
 end
