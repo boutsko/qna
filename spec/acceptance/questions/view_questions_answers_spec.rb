455c671bf26ad771338e340b2ad8fa319c15f4c3
boutsko <boutsko@gmail.com>
2015-03-25 11:01:56 +0300

    green scenario 'A visitor can see all questions'

--- spec/acceptance/questions/view_questions_answers_spec.rb
+++ spec/acceptance/questions/view_questions_answers_spec.rb
@@ -7,15 +7,21 @@ I can view a list of questions
 and for a question I can see provided answers
 } do
 
-
+  question1_title = 'question title 1'
+  question2_title = 'question title 2'
+    
   given!(:user) { create(:user) }
   given!(:question) { create(:question, user: user) }
   given!(:answer) { create(:answer, question: question) }
 
   scenario 'A visitor can see all questions' do
 
+    create(:question, title: question1_title, user: user)
+    create(:question, title: question2_title, user: user)
+
     visit questions_path
-    expect(page).to have_content 'MyString'
+    expect(page).to have_content 'question title 1'
+    expect(page).to have_content 'question title 2'
   end
 
   scenario 'A visitor can see all answers for a question' do
