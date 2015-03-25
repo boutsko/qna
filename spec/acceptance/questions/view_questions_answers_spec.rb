e0981e75fcd43087e0f326ca2c3a035deb6b2f06
boutsko <boutsko@gmail.com>
2015-03-25 11:22:26 +0300

    green scenarios 'A visitor can see all answers for a question' and 'A visitor can see all questions'

--- spec/acceptance/questions/view_questions_answers_spec.rb
+++ spec/acceptance/questions/view_questions_answers_spec.rb
@@ -7,26 +7,33 @@ I can view a list of questions
 and for a question I can see provided answers
 } do
 
-  question1_title = 'question title 1'
-  question2_title = 'question title 2'
-    
+  question1_title = 'question 1 title'
+  question1_body  = 'question 1 body'
+  question2_title = 'question 2 title'
+  
   given!(:user) { create(:user) }
   given!(:question) { create(:question, user: user) }
-  given!(:answer) { create(:answer, question: question) }
+  given!(:question) { create(:question, title: question1_title, body: question1_body,  user: user) }
+#  given!(:answer) { create(:answer, question: question) }
 
   scenario 'A visitor can see all questions' do
 
-    create(:question, title: question1_title, user: user)
+    #    create(:question, title: question1_title, user: user)
     create(:question, title: question2_title, user: user)
 
     visit questions_path
-    expect(page).to have_content 'question title 1'
-    expect(page).to have_content 'question title 2'
+    expect(page).to have_content 'question 1 title'
+    expect(page).to have_content 'question 2 title'
   end
 
   scenario 'A visitor can see all answers for a question' do
 
+    question.answers.create!([{body: 'answer 1'}, {body: 'answer 2', user: user}])
+    
     visit question_path(question)
-    expect(page).to have_content 'MyText'
+    expect(page).to have_content 'question 1 title'
+    expect(page).to have_content 'answer 1'
+    expect(page).to have_content 'answer 2'
+    save_and_open_page
   end
 end
