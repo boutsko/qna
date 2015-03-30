dfdd6cf7bd337d23532e4ebb014286e2bdfbe8b3
boutsko <boutsko@gmail.com>
2015-03-30 17:36:16 +0300

    add 'assignes new answer for question spec'

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -29,6 +29,10 @@ RSpec.describe QuestionsController, type: :controller do
     it 'assigns requested question to @question' do
       expect(assigns(:question)).to eq question
     end
+
+    it 'assignes new answer for question' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end 
     
     it 'renders show view' do
       expect(response).to render_template :show
