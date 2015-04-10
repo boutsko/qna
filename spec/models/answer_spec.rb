5ff47c7460f5e4dc14aa009b596b45667ecee86a
boutsko <boutsko@gmail.com>
2015-04-10 14:10:28 +0300

    add model/answers #make_best specs

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,4 +4,26 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
   it { should belong_to :question }
   it { should belong_to :user }
+
+  describe "#make_best" do
+    let(:question) { create(:question) }
+    let!(:answer) { create(:answer, question: question, best: true) }
+    let!(:other_answer) { create(:answer, question: question) }
+
+    it "should mark answer as best" do
+      expect(other_answer.best).to_not eq true
+
+      other_answer.make_best
+
+      expect(other_answer.best).to eq true
+    end
+
+    it "should have only one best answer" do
+      other_answer.make_best
+      answer.reload
+
+      expect(answer.best).to_not eq true
+      expect(other_answer.best).to eq true
+    end
+  end
 end
