308511245c3647e842e670b8983812c70a54eedc
boutsko <boutsko@gmail.com>
2015-04-10 13:52:33 +0300

    add spec 'other user tries to mark answer as best'

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe AnswersController, type: :controller do
   let!(:user) { create(:user) }
   let!(:other_user) { create(:user) }
-  let!(:question) { create(:question) }
+  let!(:question) { create(:question, user: user) }
   let(:answer) { create(:answer, question: question, user: user) }
   let(:answers) { create_list(:answer, 2, question: question, user: user) }
 
@@ -125,17 +125,29 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'PATCH #best' do
 
-    before do
-      sign_in question.user
-    end
-    
+    before { sign_in question.user }
+
     context 'Question\'s author' do
-      
+
       it 'changes answer to be the best one' do
         patch :best, id: answer, question_id: question, format: :js
         answer.reload 
         expect(answer.best).to be true 
       end 
+    end
+  end
+
+  describe 'PATCH #best' do
+
+    before { sign_in other_user }
+    
+    context 'Non author of the Question' do
+      
+      it 'tries to change answer to be the best one' do
+        patch :best, id: answer, question_id: question, format: :js
+        answer.reload 
+        expect(answer.best).to_not be true 
+      end
     end
   end
   
