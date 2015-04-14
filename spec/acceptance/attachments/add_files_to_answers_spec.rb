3e4e69b024aac75c25569c465b7a8d0ed6ca31b1
Sam Pai <sampai@sampai-108.local>
2015-04-14 12:18:47 +0400

    add several files when creating answer

---
+++ spec/acceptance/attachments/add_files_to_answers_spec.rb
@@ -0,0 +1,45 @@
+require 'acceptance_helper'
+
+feature 'Add files to answer', %q{
+  In order to illustrate my answer
+  As a answer's author
+  I'd like to be able to attach files
+}  do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given(:files) { Array[ "spec_helper.rb", "rails_helper.rb" ] }
+  
+  background do
+    sign_in(user)
+    visit question_path(question)
+  end
+
+  scenario 'User adds file when adds answer', js: true do
+    fill_in 'Your answer', with: 'My answer'
+    
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'Create Answer'
+    sleep 1
+    within '.answers' do
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    end
+  end 
+
+  scenario 'User adds several files when adds answer', js: true do
+    fill_in 'Your answer', with: 'My answer'
+
+    click_on 'Add more files'
+    
+    inputs = all('input[type="file"]')
+
+    2.times { |n| inputs[n].set("#{Rails.root}/spec/#{files[n]}") }
+
+    click_on 'Create Answer'
+
+    within '.answers' do
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
+    end
+  end 
+end
