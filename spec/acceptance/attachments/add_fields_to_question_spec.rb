f40ceb584964050ffc1c264614e8866762667112
Sam Pai <sampai@sampai-108.local>
2015-04-14 11:52:31 +0400

    add attachments to question GREN

--- spec/acceptance/attachments/add_fields_to_question_spec.rb
+++ spec/acceptance/attachments/add_fields_to_question_spec.rb
@@ -6,25 +6,29 @@ feature 'Add files to question', %q{
   I'd like to be able to attach files
 }  do
 
-  given(:user) {create(:user) }
+  given(:user) { create(:user) }
+  given!(:question) { build(:question) }
+  given(:files) { Array[ "spec_helper.rb", "rails_helper.rb" ] }
 
   background do
     sign_in(user)
     visit new_question_path
   end
 
-  scenario 'User adds file whsen asks question' do
+  scenario 'User add several files when asks question', js: true do
 
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'text text'
-    # click_on 'Choose File'
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-    sleep 3
-    click_on 'Add'
-    # save_and_open_page
 
-    attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    click_on "Add file to question"
+
+    inputs = all('input[type="file"]')
+
+    2.times { |n| inputs[n].set("#{Rails.root}/spec/#{files[n]}") }
+
     click_on 'Create'
+
+    # 2.times { |n| expect(page).to have_link files[n], href: "/uploads/attachment/file/#{n+1}/#{files[n]}"}
 
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
     expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
