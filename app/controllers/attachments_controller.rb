f5205885d9ab658eaaa62dadf3d523c317be2046
boutsko <boutsko@gmail.com>
2015-04-15 16:11:08 +0300

    move author_of? method to User model

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -8,17 +8,17 @@ class AttachmentsController < ApplicationController
 
   private
 
-    def load_attachment
-      @attachment = Attachment.find(params[:id]) if params.has_key?(:id)
-    end
+  def load_attachment
+    @attachment = Attachment.find(params[:id]) if params.has_key?(:id)
+  end
 
-    def find_parent
-      @parent = @attachment.attachable
-    end
+  def find_parent
+    @parent = @attachment.attachable
+  end
 
-    def author?
-      unless @parent.user_id == current_user.id
-        render status: 403, text: "Only author can delete his files"
-      end
+  def author?
+    unless current_user.author_of?(@parent)
+      render status: 403, text: "Only author can delete his files"
     end
+  end
 end
