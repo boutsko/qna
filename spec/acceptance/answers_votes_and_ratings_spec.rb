c056017f17698b12ad4053e29f751b321e6d4996
boutsko <boutsko@gmail.com>
2015-04-27 19:52:22 +0300

    complete answers acceptance spec

--- spec/acceptance/answers_votes_and_ratings_spec.rb
+++ spec/acceptance/answers_votes_and_ratings_spec.rb
@@ -2,7 +2,7 @@
 require 'acceptance_helper'
 
 feature 'User can vote', %q{
-  To like a question
+  To like an answer
   As a registered user
   I'd like to vote for it
 }, type: :feature, js: true do
@@ -11,37 +11,46 @@ feature 'User can vote', %q{
   given (:answer_author) { create(:user) }
   given (:question) { create(:question, user: question_author) }
   given! (:answer) { create(:answer, question: question, user: answer_author) }
+  given! (:second_answer) { create(:answer, question: question, user: question_author) }
   
   background do
     sign_in(question_author)
+    visit question_path(question)
   end
   
-  scenario 'User can vote for question/answer' do
-
-    visit question_path(question)
-    expect(page).to have_text question.body
-
+  scenario 'User can vote for answer' do
     within "#answer_#{ answer.id }" do
       expect(page).to have_text answer.body
       expect(page).to have_text 'Like'
       expect(page).to have_text 'Dislike'
       expect(page).to have_text 'Rating is: 0'
+    end
+  end
 
+  scenario 'User can vote for answer only once' do
+    within "#answer_#{ answer.id }" do
       click_link 'Like'
       expect(page).to have_text 'Rating is: 1'
       expect(page).to_not have_text 'Like'
       expect(page).to have_text 'Withdraw'
-
-     click_link 'Withdraw'
-     expect(page).to have_text 'Like'
-     expect(page).to have_text 'Dislike'
     end
   end
   
-  
-  scenario 'User can not vote for his question/answer'
-  scenario 'User can vote for question/answer only once'
-  scenario 'User can cancel his vote and re-vote'
-  scenario 'Question/answer has raiting'
+  scenario 'User can cancel his vote and re-vote' do
+    within "#answer_#{ answer.id }" do
+      click_link 'Like'
+      expect(page).to have_text 'Rating is: 1'
+      click_link 'Withdraw'
+      expect(page).to have_text 'Rating is: 0'
+      click_link 'Dislike'
+      expect(page).to have_text 'Rating is: -1'
+    end
+  end
 
+  scenario 'User can not vote for his answer' do
+    within "#answer_#{ second_answer.id }" do
+      expect(page).to_not have_text 'Like'
+      expect(page).to_not have_text 'Dislike'
+    end
+  end
 end
