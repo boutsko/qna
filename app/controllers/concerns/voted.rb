041835d573dbf3efd7030ab25b381cc9a2665d41
boutsko <boutsko@gmail.com>
2015-04-29 16:40:08 +0300

    add braces to functions definitions, move new_vote_by function to private

--- app/controllers/concerns/voted.rb
+++ app/controllers/concerns/voted.rb
@@ -3,28 +3,26 @@ module Voted
 
   included do
     before_action :load_votable_resource, only: [:like, :dislike, :withdraw_vote]
-#    before_action :check_user_can_vote, only: [:like, :dislike, :withdraw_vote]
 
     helper_method :user_can_vote_for
   end
 
   def like
-    @votable.liked_by current_user
-    # render 'layouts/votable/update'
+    @votable.liked_by(current_user)
     update_votes
   end
 
   def dislike
-    @votable.disliked_by current_user
+    @votable.disliked_by(current_user)
     update_votes
   end
 
   def withdraw_vote
-    @votable.withdraw_vote_by current_user
+    @votable.withdraw_vote_by(current_user)
     update_votes
   end
 
-  def user_can_vote_for votable
+  def user_can_vote_for(votable)
     if user_signed_in?
       votable.user_id != current_user.id
     end
@@ -37,14 +35,13 @@ module Voted
   end
 
   def check_user_can_vote
-    unless user_can_vote_for @votable
+    unless user_can_vote_for(@votable)
       render status: :forbidden
     end
   end
 
   def update_votes
     render 'layouts/votable/update'
-    # render 'layouts/votes/update'
   end
 end
   
--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -8,31 +8,33 @@ module Votable
   LIKE = 1
   DISLIKE = -1
 
-  def liked_by user
-    new_vote_by user
+  def liked_by(user)
+    new_vote_by(user)
   end
 
-  def disliked_by user
-    new_vote_by user, DISLIKE
+  def disliked_by(user)
+    new_vote_by(user, DISLIKE)
   end
 
-  def new_vote_by user, like = LIKE
-    votes.create(user: user, vote: like)
-  end
-
-  def withdraw_vote_by user
+  def withdraw_vote_by(user)
     votes.where(user: user).delete_all
   end
 
-  def has_vote_by user
+  def has_vote_by(user)
     votes.where(user: user).any?
   end
 
-  def vote_by user
+  def vote_by(user)
     votes.find_by(user: user).like
   end
 
   def rating
     votes.sum :vote
+  end
+
+  private
+
+  def new_vote_by(user, like = LIKE)
+    votes.create(user: user, vote: like)
   end
 end
