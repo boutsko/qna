69d5acc7eb8d2b87f73db1fe8ec3dbb32510d426
boutsko <boutsko@gmail.com>
2015-05-05 12:56:50 +0300

    add index to commentable_type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505094544) do
+ActiveRecord::Schema.define(version: 20150505095549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20150505094544) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
   end
+
+  add_index "comments", ["commentable_type"], name: "index_comments_on_commentable_type", using: :btree
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
