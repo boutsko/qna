3ebb8377e000bc1196ce9ce2b136c472b2cc06e6
boutsko <boutsko@gmail.com>
2015-05-06 17:11:47 +0300

    acceptance spec for answer and question comments

--- spec/acceptance/answer_and_question_comments_spec.rb
+++ spec/acceptance/answer_and_question_comments_spec.rb
@@ -18,7 +18,20 @@ feature 'User can comment', %q{
   
   scenario 'User can comment on answer' do
     within "#answer_#{ answer.id }" do
-      expect(page).to have_text "Create comment"
+      click_on 'Create comment'
+      fill_in 'Body', with: 'my comment for answer'
+      click_on 'Submit'
+      expect(page).to have_text 'my comment for answer'
     end
   end
+
+  scenario 'User can comment on question' do
+    within "#question_#{ question.id }" do
+      click_on 'Create question comment'
+      fill_in 'Body', with: 'my comment for question'
+      click_on 'Submit'
+      expect(page).to have_text 'my comment for question'
+    end
+  end
+  
 end
