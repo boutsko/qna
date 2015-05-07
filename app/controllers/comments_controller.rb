920f710e20e120d1101161a4185fcce76a19f93d
boutsko <boutsko@gmail.com>
2015-05-07 19:03:03 +0300

    backtrack: comments rendered via item

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
+# coding: utf-8
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :load_commentable, only: :create
   before_action :load_comment, only: :destroy
 
   def create
-    @comment = @commentable.comments.create comment_params
+    @comment = @commentable.comments.create(comment_params)
+    # @comment.save
   end
 
   def destroy
@@ -14,8 +16,9 @@ class CommentsController < ApplicationController
   private
 
   def load_commentable
-    klass = [Question, Answer].detect{|c| params["#{c.name.underscore}_id"]}
-    @commentable= klass.find(params["#{klass.name.underscore}_id"])
+    # klass = [Question, Answer].detect{|c| params["#{c.name.underscore}_id"]}
+    # @commentable= klass.find(params["#{klass.name.underscore}_id"])
+    @commentable = commentable_klass.find(params[commentable_id_s])
 
   end
 
@@ -27,4 +30,21 @@ class CommentsController < ApplicationController
     strong_params = params.require(:comment).permit(:body, :commentable)
   end
 
+  def commentable_name
+    params[:commentable]
+  end
+  
+  def commentable_id_s
+    "#{commentable_name}_id"
+  end
+  
+  
+  def commentable_klass
+    params[:commentable].classify.constantize
+  end
+
+  
 end
+
+
+#Parameters: {"utf8"=>"✓", "comment"=>{"body"=>"aaa"}, "commit"=>"Submit", "commentable"=>"answer", "answer_id"=>"1"}
