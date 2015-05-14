2ff45ef7150e4b2e606e67f9c31f6ef03558c5c7
boutsko <boutsko@gmail.com>
2015-05-14 16:52:01 +0300

    add sign_in around_action for both facebook and twitter

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,23 +1,24 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  around_action :sign_in_all
+
   def facebook
-    #render json: request.env['omniauth.auth']
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user.persisted?
-      sign_in_and_redirect @user,event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    end
   end
 
   def twitter
-    # render json: request.env['omniauth.auth']
+  end
+
+  private
+
+  def sign_in_all
     @user = User.find_for_oauth(request.env['omniauth.auth'])
+    auth = request.env['omniauth.auth']
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: auth.provider.capitalize) if is_navigational_format?
     else
       session["devise.omniauth"] = request.env['omniauth.auth'].except('extra')
       redirect_to new_omniauth_registration_path
     end
   end
-
+  
 end
