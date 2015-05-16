dbaa308f9f0856a6fd3f6478b00b3c74d10a2217
Sam Pai <sampai@sampai-108.local>
2015-05-17 00:26:31 +0400

    refactor

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,18 +1,31 @@
 class Ability
   include CanCan::Ability
 
+  attr_reader :user
+
   def initialize(user)
+    @user = user
+    
     if user
-      if user.admin?
-        can :manage, :all
-      else
-        can :read, :all
-        can :create, [ Question, Answer, Comment ]
-        can :update, Question,  user: user
-        can :update, Answer,  user: user
-        can :update, Comment
-      end
+      user.admin? ? admin_abilities : user_abilities
+    else
+      guest_abilities
     end
+  end
+
+  def guest_abilities
     can :read, :all
   end
+  
+  def user_abilities
+    guest_abilities
+    can :create, [ Question, Answer, Comment ]
+    can :update, [ Question, Answer ], user: user
+    can :update, Comment
+  end
+  
+  def admin_abilities
+    can :manage, :all
+  end
+
 end
