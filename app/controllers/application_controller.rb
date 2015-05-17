5abf1d65444828793339afcb6333854f37eac9af
boutsko <boutsko@gmail.com>
2015-05-18 02:18:35 +0300

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # rescue_from CanCan::AccessDenied do |exeption|
-  #   redirect_to root_path, alert: exeption.message
-  # end
+  rescue_from CanCan::AccessDenied do |exeption|
+    redirect_to root_path, alert: exeption.message
+  end
   
 end
