c11f7b380667147cb511317e31720d6019d3b52d
boutsko <boutsko@gmail.com>
2015-05-22 09:28:18 +0300

    add actions for all users

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -7,6 +7,13 @@ class Api::V1::ProfilesController < ApplicationController
     respond_with current_resource_owner
   end
 
+  def index
+    respond_with(User.where.not(id: current_resource_owner.id))
+  end
+
+  def show
+  end
+
   private
   def current_resource_owner
     @current_resource_owner ||=User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
