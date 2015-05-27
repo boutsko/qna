3836ec03ea054195836d7b44730037d2f3349e45
boutsko <boutsko@gmail.com>
2015-05-27 11:05:27 +0300

    correct 'answer relates to question' spec

--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -126,7 +126,7 @@ describe 'Answers API' do
 
       it 'answer relates to question' do
         post_create
-        expect(assigns(:question)).to eq question
+        expect(question.answers.last.as_json[:question_id]).to match(question.as_json[:id])
       end
 	end
   end
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -81,8 +81,6 @@ describe 'Questions API' do
 
         it 'relates to user' do
           post_create
-          puts response.body
-          puts "current_resource_owner: #{access_token.resource_owner_id}"
           expect(response.body).to be_json_eql(access_token.resource_owner_id).at_path("question/user_id")
         end
 	  end
