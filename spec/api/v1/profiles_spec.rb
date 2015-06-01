c1acc0d3f56c4fe14360bf43d7645d68694cf43e
boutsko <boutsko@gmail.com>
2015-06-02 01:57:18 +0300

    add and deploy 'API Authenticable' shared_example

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -1,68 +1,36 @@
 require 'rails_helper'
 
 describe 'Profile API' do
-  #--
-  let(:api_path) { 'api_v1_questions_path' }
+
+  let(:api_path) { '/api/v1/profiles/me' }
+
   it_behaves_like "API Authenticable"
-  
-  context 'authorized' do
-    let!(:me) { create :user }
-    let!(:users) { create_list :user, 3 }
-    let(:access_token) { create :access_token, resource_owner_id: me.id }
 
-    before { get '/api/v1/profiles/', format: :json, access_token: access_token.token }
+  describe 'GET /me' do
+    context 'authorized' do
+      let(:me) { create(:user) }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
-    it 'returns status ok' do
-      expect(response).to be_success
-    end
-
-    it 'returns list of users except resource owner' do
-      expect(response.body).to be_json_eql(users.to_json)
-      expect(response.body).to_not include_json(me.to_json)
-    end
-  end
-end
-#--
-
-
-describe 'GET /me' do
-  context 'unauthorized' do
-    it 'returns 401 status if there is no access_token' do
-      get '/api/v1/profiles/me', format: :json
-      expect(response.status).to eq 401
-    end
-
-    it 'returns 401 status if access_token is invalid ' do
-      get '/api/v1/profiles/me', format: :json, access_token: '1234'
-      expect(response.status).to eq 401
-    end
-  end
-
-  
-  context 'authorized' do
-    let(:me) { create(:user) }
-    let(:access_token) { create(:access_token, resource_owner_id: me.id) }
-
-    before { get '/api/v1/profiles/me', format: :json, access_token: access_token.token }
-    
-    it 'returns 200 status' do
-      expect(response).to be_success
-    end
-
-
-    %w(email id created_at updated_at).each do |attr|
+      before { get '/api/v1/profiles/me', format: :json, access_token: access_token.token }
       
-      it "contains #{attr}" do
-        expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr)
+      it 'returns 200 status' do
+        expect(response).to be_success
       end
-    end
-    
-    %w(password encrypted_password).each do |attr|
+
+
+      %w(email id created_at updated_at).each do |attr|
+        
+        it "contains #{attr}" do
+          expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr)
+        end
+      end
       
-      it "does not contains #{attr} " do
-        expect(response.body).to_not have_json_path(attr)
+      %w(password encrypted_password).each do |attr|
+        
+        it "does not contains #{attr} " do
+          expect(response.body).to_not have_json_path(attr)
+        end
       end
     end
   end
 end
-
