10d87e0afa9839e12b3856ffd00831ed7fd57f69
boutsko <boutsko@gmail.com>
2015-06-01 17:10:45 +0300

    clean  specs to correspond thin controllers

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -60,10 +60,6 @@ RSpec.describe QuestionsController, type: :controller do
     it 'assigns requested question to @question' do
       expect(assigns(:question)).to eq question
     end
-
-    it 'renders edit view' do
-      expect(response).to render_template :edit
-    end
   end
 
   describe 'POST #create' do
