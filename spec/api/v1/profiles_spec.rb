396acac44385db77a7b669ed7bfe7769a19595ab
boutsko <boutsko@gmail.com>
2015-06-02 16:29:25 +0300

    complete profiles_spec with def do_request

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Profile API' do
+describe 'Profiles API' do
 
   let(:api_path) { '/api/v1/profiles/me' }
 
@@ -12,25 +12,29 @@ describe 'Profile API' do
       let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
       before { get '/api/v1/profiles/me', format: :json, access_token: access_token.token }
-      
+
       it 'returns 200 status' do
         expect(response).to be_success
       end
 
 
       %w(email id created_at updated_at).each do |attr|
-        
+
         it "contains #{attr}" do
           expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr)
         end
       end
-      
+
       %w(password encrypted_password).each do |attr|
-        
+
         it "does not contains #{attr} " do
           expect(response.body).to_not have_json_path(attr)
         end
       end
     end
   end
+  def do_request(options = {})
+    get api_path, { format: :json }.merge(options)
+  end
+
 end
--- spec/support/shared/api_authorization.rb
+++ spec/support/shared/api_authorization.rb
@@ -3,12 +3,12 @@ shared_examples_for "API Authenticable" do
   context 'unauthorized' do
     
     it 'returns 401 status if there is no access_token' do
-      get api_path, format: :json
+      do_request
       expect(response.status).to eq 401
     end
 
     it 'returns 401 status if access_token is invalid ' do
-      get api_path, format: :json, access_token: '1234'
+      do_request(access_token: '1234')
       expect(response.status).to eq 401
     end
   end
