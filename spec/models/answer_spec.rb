14b6dc7641f24de8a586e1931c73c72e8450f384
boutsko <boutsko@gmail.com>
2015-06-02 17:10:55 +0300

    add shared example like "attachable" check to answer model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
   it { should belong_to :user }
-  it { should have_many :attachments }
   it { should have_many :votes }
 
   it { should validate_presence_of :body }
 
-  it { should accept_nested_attributes_for :attachments }
-  
+  it_should_behave_like "attachable"
+    
   describe "#make_best" do
     let(:question) { create(:question) }
     let!(:answer) { create(:answer, question: question, best: true) }
--- spec/support/shared/attachable.rb
+++ spec/support/shared/attachable.rb
@@ -1,60 +1,6 @@
 shared_examples_for "attachable" do
 
-  # let(:path) { new_question_path }
-  # let(:attachable) { question }
-
-
-
-  # let(:user) { create(:user) }
-  # let!(:question) { build(:question) }
-  # let(:files) { Array[ "spec_helper.rb", "rails_helper.rb" ] }
-
-  # background do
-  #   sign_in(user)
-  #   visit path
-  # end
-
-  scenario "User adds file when sends question", js: true do
-
-    fill_up_form
-    
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-    click_on 'Create'
-    sleep 1
-    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
-  end 
-
-  scenario "User adds files when sends question", js: true do
-
-    fill_up_form
-    
-    click_on "Add file to question"
-
-    inputs = all('input[type="file"]')
-
-    2.times { |n| inputs[n].set("#{Rails.root}/spec/#{files[n]}") }
-
-    click_on 'Create'
-
-    # 2.times { |n| expect(page).to have_link files[n], href: "/uploads/attachment/file/#{n+1}/#{files[n]}"}
-
-    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
-    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
-    
-  end
-  scenario 'User adds and removes file when asks question', js: true do
-
-    fill_up_form
-    
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-    click_on 'Delete File'
-    click_on 'Create'
-    expect(page).to_not have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
-  end
+  it { should accept_nested_attributes_for :attachments }
+  it { should have_many :attachments }
 
 end
-
-# def fill_up_form
-#   fill_in 'Title', with: 'Test question'
-#   fill_in 'Body', with: 'text text'
-# end
