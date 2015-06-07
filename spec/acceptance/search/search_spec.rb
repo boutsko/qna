3c26ab270deaa112bd2e55d055c6946a50d2a141
boutsko <boutsko@gmail.com>
2015-06-07 23:29:31 +0300

    add spec

---
+++ spec/acceptance/search/search_spec.rb
@@ -0,0 +1,42 @@
+require 'acceptance_helper'
+
+feature 'User may search content', %q{
+        In order to be able search content
+        As an user
+        I want to be able search content
+} do
+  given!(:user) { create(:user) }
+  given!(:question) { create(:question, title: 'find me', body: 'foo') }
+  given!(:answer) { create(:answer, question: question, body: 'test search') }
+  
+  scenario 'search selected item', js: true do
+    
+    ThinkingSphinx::Test.run do
+	  sign_in(user)
+      
+      select('Question', from: 'conditions')
+      fill_in 'search', with: 'foo'
+      click_on 'Search'
+    
+	  expect(page).to have_content  question.body
+	  expect(page).to have_content  question.title
+    end  
+  end
+
+  scenario 'search all', js: true do
+    
+    ThinkingSphinx::Test.run do
+	  sign_in(user)
+      
+      select('All', from: 'conditions')
+      fill_in 'search', with: ''
+      click_on 'Search'
+
+	  expect(page).to have_content  user.email
+	  expect(page).to have_content  question.title
+	  expect(page).to have_content  answer.body
+    end  
+  end
+
+
+end
--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
+require 'thinking_sphinx/test'
 
 RSpec.configure do |config|
   Capybara.javascript_driver = :webkit
-  
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with :truncation
+    ThinkingSphinx::Test.init
+    ThinkingSphinx::Test.start_with_autostop
+  end
+
   config.include AcceptanceController, type: :feature
 
   config.use_transactional_fixtures = false
@@ -26,4 +33,5 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
+
 end
